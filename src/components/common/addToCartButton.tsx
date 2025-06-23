08b7417eccf1de6859275c19bd5544d00a437b68
phamnguyen1700 <169217913+phamnguyen1700@users.noreply.github.com>
2025-06-23 13:58:31 +0700

    UI addcartButton

--- src/components/common/addToCartButton.tsx
+++ src/components/common/addToCartButton.tsx
@@ -1,53 +1,52 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import Icon from "@/components/assests/icons";
-import { IProduct } from "@/types/product";
+import { ISku } from "@/types/product";
 import { CART_STORAGE_KEY } from "@/constants/storageKey";
 import { toast } from "react-toastify";
 import { cn } from "@/lib/utils";
 
 interface AddToCartButtonProps {
-  variant?: 'card' | 'detail';
-  product: IProduct;
+  selectedSku: ISku;
+  quantity: number;
 }
 
-export const AddToCartButton: React.FC<AddToCartButtonProps> = ({ product, variant }) => {
+export const AddToCartButton: React.FC<AddToCartButtonProps> = ({
+  selectedSku,
+  quantity,
+}) => {
   const handleAddToCart = () => {
     const cart = JSON.parse(localStorage.getItem(CART_STORAGE_KEY) || "[]");
 
-    const existingIndex = cart.findIndex((item: { product: string }) => item.product === product._id);
+    const existingIndex = cart.findIndex(
+      (item: { skuId: string }) => item.skuId === selectedSku._id
+    );
 
     if (existingIndex !== -1) {
-      cart[existingIndex].quantity += 1;
+      cart[existingIndex].quantity += quantity;
     } else {
       cart.push({
-        product: product._id,
-        name: product.name,
-        image: product.image[0],
-        quantity: 1,
+        skuId: selectedSku._id,
+        productId: selectedSku.productId,
+        name: selectedSku.variantName,
+        image: selectedSku.image,
+        quantity: quantity,
       });
     }
 
     localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(cart));
-
     window.dispatchEvent(new Event("cartUpdated"));
     toast.success("Thêm mới sản phẩm thành công!");
   };
 
   return (
     <Button
-      variant="ghost"
       onClick={handleAddToCart}
       className={cn(
-        "flex items-center gap-2 bg-white text-black rounded-none hover:bg-stone-200",
-        variant === "card" && "w-16",
-        variant === "detail" && "w-full"
+        "bg-[var(--tertiary)] text-white h-[44px] w-[160px] text-[15px] font-medium tracking-wide rounded-none hover:bg-[var(--primary)]",
       )}
-
     >
-      <Icon name="shoppingBasket" className="w-5 h-5" />
+      Add to Cart
     </Button>
   );
 };
-
